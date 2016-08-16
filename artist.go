ae4202df6ab3d3761ed5bf5a461ba0a8ec1f285c
Chemicxlkid <chemica7x@gmail.com>
2016-08-16 10:48:15 -0700

    Add files via upload

---
+++ artist.go
@@ -0,0 +1,212 @@
+package spotify
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+// SimpleArtist contains basic info about an artist.
+type SimpleArtist struct {
+	Name string `json:"name"`
+	ID   ID     `json:"id"`
+	// The Spotify URI for the artist.
+	URI URI `json:"uri"`
+	// A link to the Web API enpoint providing full details of the artist.
+	Endpoint     string            `json:"href"`
+	ExternalURLs map[string]string `json:"external_urls"`
+}
+
+// FullArtist provides extra artist data in addition to what is provided by SimpleArtist.
+type FullArtist struct {
+	SimpleArtist
+	// The popularity of the artist, expressed as an integer between 0 and 100.
+	// The artist's popularity is calculated from the popularity of the artist's tracks.
+	Popularity int `json:"popularity"`
+	// A list of genres the artist is associated with.  For example, "Prog Rock"
+	// or "Post-Grunge".  If not yet classified, the slice is empty.
+	Genres    []string `json:"genres"`
+	Followers Followers
+	// Images of the artist in various sizes, widest first.
+	Images []Image `json:"images"`
+}
+
+// GetArtist is a wrapper around DefaultClient.GetArtist.
+func GetArtist(id ID) (*FullArtist, error) {
+	return DefaultClient.GetArtist(id)
+}
+
+// GetArtist gets Spotify catalog information for a single artist, given its Spotify ID.
+func (c *Client) GetArtist(id ID) (*FullArtist, error) {
+	spotifyURL := fmt.Sprintf("%sartists/%s", baseAddress, id)
+	resp, err := c.http.Get(spotifyURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, decodeError(resp.Body)
+	}
+	var a FullArtist
+	err = json.NewDecoder(resp.Body).Decode(&a)
+	if err != nil {
+		return nil, err
+	}
+	return &a, nil
+}
+
+// GetArtists is a wrapper around DefaultClient.GetArtists.
+func GetArtists(ids ...ID) ([]*FullArtist, error) {
+	return DefaultClient.GetArtists(ids...)
+}
+
+// GetArtists gets spotify catalog information for several artists based on their
+// Spotify IDs.  It supports up to 50 artists in a single call.  Artists are
+// returned in the order requested.  If an artist is not found, that position
+// in the result will be nil.  Duplicate IDs will result in duplicate artists
+// in the result.
+func (c *Client) GetArtists(ids ...ID) ([]*FullArtist, error) {
+	spotifyURL := fmt.Sprintf("%sartists?ids=%s", baseAddress, strings.Join(toStringSlice(ids), ","))
+	resp, err := c.http.Get(spotifyURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, decodeError(resp.Body)
+	}
+	var a struct {
+		Artists []*FullArtist
+	}
+	err = json.NewDecoder(resp.Body).Decode(&a)
+	if err != nil {
+		return nil, err
+	}
+	return a.Artists, nil
+}
+
+// GetArtistsTopTracks is a wrapper around DefaultClient.GetArtistsTopTracks.
+func GetArtistsTopTracks(artistID ID, country string) ([]FullTrack, error) {
+	return DefaultClient.GetArtistsTopTracks(artistID, country)
+}
+
+// GetArtistsTopTracks gets Spotify catalog information about an artist's top
+// tracks in a particular country.  It returns a maximum of 10 tracks.  The
+// country is specified as an ISO 3166-1 alpha-2 country code.
+func (c *Client) GetArtistsTopTracks(artistID ID, country string) ([]FullTrack, error) {
+	spotifyURL := fmt.Sprintf("%sartists/%s/top-tracks?country=%s", baseAddress, artistID, country)
+	resp, err := c.http.Get(spotifyURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, decodeError(resp.Body)
+	}
+	var t struct {
+		Tracks []FullTrack `json:"tracks"`
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&t)
+	if err != nil {
+		return nil, err
+	}
+	return t.Tracks, nil
+}
+
+// GetRelatedArtists is a wrapper around DefaultClient.GetRelatedArtists.
+func GetRelatedArtists(id ID) ([]FullArtist, error) {
+	return DefaultClient.GetRelatedArtists(id)
+}
+
+// GetRelatedArtists gets Spotify catalog information about artists similar to a
+// given artist.  Similarity is based on analysis of the Spotify community's
+// listening history.  This function returns up to 20 artists that are considered
+// related to the specified artist.
+func (c *Client) GetRelatedArtists(id ID) ([]FullArtist, error) {
+	spotifyURL := fmt.Sprintf("%sartists/%s/related-artists", baseAddress, id)
+	resp, err := c.http.Get(spotifyURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, decodeError(resp.Body)
+	}
+	var a struct {
+		Artists []FullArtist `json:"artists"`
+	}
+	err = json.NewDecoder(resp.Body).Decode(&a)
+	if err != nil {
+		return nil, err
+	}
+	return a.Artists, nil
+}
+
+// GetArtistAlbums is a wrapper around DefaultClient.GetArtistAlbums.
+func GetArtistAlbums(artistID ID) (*SimpleAlbumPage, error) {
+	return DefaultClient.GetArtistAlbums(artistID)
+}
+
+// GetArtistAlbums gets Spotify catalog information about an artist's albums.
+// It is equivalent to GetArtistAlbumsOpt(artistID, nil).
+func (c *Client) GetArtistAlbums(artistID ID) (*SimpleAlbumPage, error) {
+	return c.GetArtistAlbumsOpt(artistID, nil, nil)
+}
+
+// GetArtistAlbumsOpt is a wrapper around DefaultClient.GetArtistAlbumsOpt
+func GetArtistAlbumsOpt(artistID ID, options *Options, t *AlbumType) (*SimpleAlbumPage, error) {
+	return DefaultClient.GetArtistAlbumsOpt(artistID, options, t)
+}
+
+// GetArtistAlbumsOpt is just like GetArtistAlbums, but it accepts optional
+// parameters used to filter and sort the result.
+//
+// The AlbumType argument can be used to find a particular type of album.  Search
+// for multiple types by OR-ing the types together.
+func (c *Client) GetArtistAlbumsOpt(artistID ID, options *Options, t *AlbumType) (*SimpleAlbumPage, error) {
+	spotifyURL := fmt.Sprintf("%sartists/%s/albums", baseAddress, artistID)
+	// add optional query string if options were specified
+	values := url.Values{}
+	if t != nil {
+		values.Set("album_type", t.encode())
+	}
+	if options != nil {
+		if options.Country != nil {
+			values.Set("market", *options.Country)
+		} else {
+			// if the market is not specified, Spotify will likely return a lot
+			// of duplicates (one for each market in which the album is available)
+			// - prevent this behavior by falling back to the US by default
+			// TODO: would this ever be the desired behavior?
+			values.Set("market", CountryUSA)
+		}
+		if options.Limit != nil {
+			values.Set("limit", strconv.Itoa(*options.Limit))
+		}
+		if options.Offset != nil {
+			values.Set("offset", strconv.Itoa(*options.Offset))
+		}
+	}
+	if query := values.Encode(); query != "" {
+		spotifyURL += "?" + query
+	}
+	resp, err := c.http.Get(spotifyURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, decodeError(resp.Body)
+	}
+	var p SimpleAlbumPage
+	err = json.NewDecoder(resp.Body).Decode(&p)
+	if err != nil {
+		return nil, err
+	}
+	return &p, nil
+}
---
+++ user.go
@@ -0,0 +1,354 @@
+package spotify
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+// User contains the basic, publicly available information about a Spotify user.
+type User struct {
+	// The name displayed on the user's profile.
+	// Note: Spotify currently fails to populate
+	// this field when querying for a playlist.
+	DisplayName string `json:"display_name"`
+	// Known public external URLs for the user.
+	ExternalURLs map[string]string `json:"external_urls"`
+	// Information about followers of the user.
+	Followers Followers `json:"followers"`
+	// A link to the Web API endpoint for this user.
+	Endpoint string `json:"href"`
+	// The Spotify user ID for the user.
+	ID string `json:"id"`
+	// The user's profile image.
+	Images []Image `json:"images"`
+	// The Spotify URI for the user.
+	URI URI `json:"uri"`
+}
+
+// PrivateUser contains additional information about a user.
+// This data is private and requires user authentication.
+type PrivateUser struct {
+	User
+	// The country of the user, as set in the user's account profile.
+	// An ISO 3166-1 alpha-2 country code.  This field is only available when the
+	// current user has granted acess to the ScopeUserReadPrivate scope.
+	Country string `json:"country"`
+	// The user's email address, as entered by the user when creating their account.
+	// Note: this email is UNVERIFIED - there is no proof that it actually
+	// belongs to the user.  This field is only available when the current user
+	// has granted access to the ScopeUserReadEmail scope.
+	Email string `json:"email"`
+	// The user's Spotify subscription level: "premium", "free", etc.
+	// The subscription level "open" can be considered the same as "free".
+	// This field is only available when the current user has granted access to
+	// the ScopeUserReadPrivate scope.
+	Product string `json:"product"`
+	// The user's date of birth, in the format 'YYYY-MM-DD'.  You can use
+	// the DateLayout constant to convert this to a time.Time value.
+	// This field is only available when the current user has granted
+	// access to the ScopeUserReadBirthdate scope.
+	Birthdate string `json:"birthdate"`
+}
+
+// GetUsersPublicProfile is a wrapper around DefaultClient.GetUsersPublicProfile.
+func GetUsersPublicProfile(userID ID) (*User, error) {
+	return DefaultClient.GetUsersPublicProfile(userID)
+}
+
+// GetUsersPublicProfile gets public profile information about a
+// Spotify User.  It does not require authentication.
+func (c *Client) GetUsersPublicProfile(userID ID) (*User, error) {
+	spotifyURL := baseAddress + "users/" + string(userID)
+	resp, err := c.http.Get(spotifyURL)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, decodeError(resp.Body)
+	}
+	var user User
+	err = json.NewDecoder(resp.Body).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+// CurrentUser gets detailed profile information about the
+// current user.  This call requires authorization.
+//
+// Reading the user's email address requires that the application
+// has the ScopeUserReadEmail scope.  Reading the country, display
+// name, profile images, and product subscription level requires
+// that the application has the ScopeUserReadPrivate scope.
+//
+// Warning: The email address in the response will be the address
+// that was entered when the user created their spotify account.
+// This email address is unverified - do not assume that Spotify has
+// checked that the email address actually belongs to the user.
+func (c *Client) CurrentUser() (*PrivateUser, error) {
+	resp, err := c.http.Get(baseAddress + "me")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, decodeError(resp.Body)
+	}
+	var result PrivateUser
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+// CurrentUsersTracks gets a list of songs saved in the current
+// Spotify user's "Your Music" library.
+func (c *Client) CurrentUsersTracks() (*SavedTrackPage, error) {
+	return c.CurrentUsersTracksOpt(nil)
+}
+
+// CurrentUsersTracksOpt is like CurrentUsersTracks, but it accepts additional
+// options for sorting and filtering the results.
+func (c *Client) CurrentUsersTracksOpt(opt *Options) (*SavedTrackPage, error) {
+	spotifyURL := baseAddress + "me/tracks"
+	if opt != nil {
+		v := url.Values{}
+		if opt.Country != nil {
+			v.Set("country", *opt.Country)
+		}
+		if opt.Limit != nil {
+			v.Set("limit", strconv.Itoa(*opt.Limit))
+		}
+		if opt.Offset != nil {
+			v.Set("offset", strconv.Itoa(*opt.Offset))
+		}
+		if params := v.Encode(); params != "" {
+			spotifyURL += "?" + params
+		}
+	}
+	resp, err := c.http.Get(spotifyURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, decodeError(resp.Body)
+	}
+	var result SavedTrackPage
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+// Follow adds the current user as a follower of one or more
+// artists or other spotify users, identified by their Spotify IDs.
+// This call requires authorization.
+//
+// Modifying the lists of artists or users the current user follows
+// requires that the application has the ScopeUserFollowModify scope.
+func (c *Client) Follow(ids ...ID) error {
+	return c.modifyFollowers(true, ids...)
+}
+
+// Unfollow removes the current user as a follower of one or more
+// artists or other Spotify users.  This call requires authorization.
+//
+// Modifying the lists of artists or users the current user follows
+// requires that the application has the ScopeUserFollowModify scope.
+func (c *Client) Unfollow(ids ...ID) error {
+	return c.modifyFollowers(false, ids...)
+}
+
+// CurrentUserFollows checks to see if the current user is following
+// one or more artists or other Spotify Users.  This call requires
+// authorization, and that the application has the ScopeUserFollowRead
+// scope.
+//
+// The t argument indicates the type of the IDs, and must be either
+// "user" or "artist".
+//
+// The result is returned as a slice of bool values in the same order
+// in which the IDs were specified.
+func (c *Client) CurrentUserFollows(t string, ids ...ID) ([]bool, error) {
+	if l := len(ids); l == 0 || l > 50 {
+		return nil, errors.New("spotify: UserFollows supports 1 to 50 IDs")
+	}
+	if t != "artist" && t != "user" {
+		return nil, errors.New("spotify: t must be 'artist' or 'user'")
+	}
+	spotifyURL := fmt.Sprintf("%sme/following/contains?type=%s&ids=%s",
+		baseAddress, t, strings.Join(toStringSlice(ids), ","))
+	resp, err := c.http.Get(spotifyURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, decodeError(resp.Body)
+	}
+	var result []bool
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (c *Client) modifyFollowers(follow bool, ids ...ID) error {
+	if l := len(ids); l == 0 || l > 50 {
+		return errors.New("spotify: Follow/Unfollow supports 1 to 50 IDs")
+	}
+	spotifyURL := baseAddress + "me/following?" + strings.Join(toStringSlice(ids), ",")
+	method := "PUT"
+	if !follow {
+		method = "DELETE"
+	}
+	req, err := http.NewRequest(method, spotifyURL, nil)
+	if err != nil {
+		return err
+	}
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusNoContent {
+		return decodeError(resp.Body)
+	}
+	return nil
+}
+
+// CurrentUsersFollowedArtists gets the current user's followed artists.
+// This call requires authorization, and that the user has granted the
+// ScopeUserFollowRead scope.
+func (c *Client) CurrentUsersFollowedArtists() (*FullArtistCursorPage, error) {
+	return c.CurrentUsersFollowedArtistsOpt(-1, "")
+}
+
+// CurrentUsersFollowedArtistsOpt is like CurrentUsersFollowedArtists,
+// but it accept the optional arguments limit and after.  Limit is the
+// maximum number of items to return (1 <= limit <= 50), and after is
+// the last artist ID retrieved from the previous request.  If you don't
+// wish to specify either of the parameters, use -1 for limit and the empty
+// string for after.
+func (c *Client) CurrentUsersFollowedArtistsOpt(limit int, after string) (*FullArtistCursorPage, error) {
+	spotifyURL := baseAddress + "me/following?type=artist"
+	v := url.Values{}
+	if limit != -1 {
+		v.Set("limit", strconv.Itoa(limit))
+	}
+	if after != "" {
+		v.Set("after", after)
+	}
+	if params := v.Encode(); params != "" {
+		spotifyURL += "?" + params
+	}
+	resp, err := c.http.Get(spotifyURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, decodeError(resp.Body)
+	}
+	var result struct {
+		A FullArtistCursorPage `json:"artists"`
+	}
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+	return &result.A, nil
+}
+
+// CurrentUsersAlbums gets a list of albums saved in the current
+// Spotify user's "Your Music" library.
+func (c *Client) CurrentUsersAlbums() (*SavedAlbumPage, error) {
+	return c.CurrentUsersAlbumsOpt(nil)
+}
+
+// CurrentUsersAlbumsOpt is like CurrentUsersAlbums, but it accepts additional
+// options for sorting and filtering the results.
+func (c *Client) CurrentUsersAlbumsOpt(opt *Options) (*SavedAlbumPage, error) {
+	spotifyURL := baseAddress + "me/albums"
+	if opt != nil {
+		v := url.Values{}
+		if opt.Country != nil {
+			v.Set("market", *opt.Country)
+		}
+		if opt.Limit != nil {
+			v.Set("limit", strconv.Itoa(*opt.Limit))
+		}
+		if opt.Offset != nil {
+			v.Set("offset", strconv.Itoa(*opt.Offset))
+		}
+		if params := v.Encode(); params != "" {
+			spotifyURL += "?" + params
+		}
+	}
+	resp, err := c.http.Get(spotifyURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, decodeError(resp.Body)
+	}
+	var result SavedAlbumPage
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+// CurrentUsersPlaylists gets a list of the playlists owned or followed by
+// the current spotify user.  This call requires authorization.
+// Private playlists require the ScopePlaylistReadPrivate scope.  Note that
+// this scope alone will not return collaborative playlists, even though
+// they are always private.  In order to retrieve collaborative playlists
+// the user must authorize the ScopePlaylistReadCollaborative scope.
+func (c *Client) CurrentUsersPlaylists() (*SimplePlaylistPage, error) {
+	return c.CurrentUsersPlaylistsOpt(nil)
+}
+
+// CurrentUsersPlaylistsOpt is like CurrentUsersPlaylists, but it accepts
+// additional options for sorting and filtering the results.
+func (c *Client) CurrentUsersPlaylistsOpt(opt *Options) (*SimplePlaylistPage, error) {
+	spotifyURL := baseAddress + "me/playlists"
+	if opt != nil {
+		v := url.Values{}
+		if opt.Limit != nil {
+			v.Set("limit", strconv.Itoa(*opt.Limit))
+		}
+		if opt.Offset != nil {
+			v.Set("offset", strconv.Itoa(*opt.Offset))
+		}
+		if params := v.Encode(); params != "" {
+			spotifyURL += "?" + params
+		}
+	}
+	resp, err := c.http.Get(spotifyURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, decodeError(resp.Body)
+	}
+	var result SimplePlaylistPage
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
